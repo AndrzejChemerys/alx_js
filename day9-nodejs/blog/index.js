f2064d15e08630390a9ad91ab3f57b88e68b1c0d
AndriiChemerys <andriichemerys@gmail.com>
2020-03-07 16:59:49 +0100

    nodejs basics

---
+++ day9-nodejs/blog/index.js
@@ -0,0 +1,23 @@
+//importujemy moduły do naszego serwera
+
+let express = require('express');
+let bodyParser = require('body-parser');
+
+//tworzymy obiekt aplikacji serwera z modułu express
+let app = express();
+
+//zdefiniowanie metod które będą wukorzystywane przez serwer
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//uruchomienie silnika szablonów dla modułu ejs
+app.set('view engine', 'ejs');
+
+//uruchomienie serwera na porcie 8080
+app.listen(8080);
+console.log('Serwer pomyślnie uruchomiony na porcie 8080');
+
+app.get('/', function(req, res) {
+	res.render('index', {imie:"Robert"});
+});

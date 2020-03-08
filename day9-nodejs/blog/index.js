5c5d8e8f01dabf3213d6bc7d6cf2df9dfc50f6ae
AndriiChemerys <andriichemerys@gmail.com>
2020-03-08 11:12:28 +0100

    blog app with nodejs

--- day9-nodejs/blog/index.js
+++ day9-nodejs/blog/index.js
@@ -14,6 +14,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 //uruchomienie silnika szablonów dla modułu ejs
 app.set('view engine', 'ejs');
 
+//definicja ścieżki serwera
+app.use(express.static(__dirname + '/'));
+
 //uruchomienie serwera na porcie 8080
 app.listen(8080);
 console.log('Serwer pomyślnie uruchomiony na porcie 8080');
@@ -54,4 +57,25 @@ app.get('/delete/:id', function(req, res) {
 	let id = req.params.id;
 	posts.splice(id, 1);
 	res.render('index', {posts: posts});
+});
+
+app.get('/editPost/:id', function(req, res) {
+
+	let id = req.params.id;
+
+	res.render('editPost', {post: posts[id], id:id});
+});
+
+app.post('/editPost/:id', function(req, res) {
+	let id = req.params.id;
+
+	let obj = {
+		title: req.body['title'],
+		description: req.body['description']
+	}
+
+	posts[id] = obj;
+
+	res.render('index', {posts: posts});
+
 });
